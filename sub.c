b6b059c45b75ea4fa0671dad534d4e93a6eb8dea
brucetravis <bruceambundo@gmail.com>
2023-08-24 11:48:15 -0700

    sub.c

---
+++ sub.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+  *sub_s - Carrys out sustration
+  *@head: The stack head
+  *@counter: The line_number
+  *Return: no return
+ */
+void sub_s(stack_t **head, unsigned int counter)
+{
+	int sus, nodes;
+	stack_t *aux;
+
+	aux = *head;
+	for (nodes = 0; aux != NULL; nodes++)
+		aux = aux->next;
+	if (nodes < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stacks(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = *head;
+	sus = aux->next->n - aux->n;
+	aux->next->n = sus;
+	*head = aux->next;
+	free(aux);
+}
